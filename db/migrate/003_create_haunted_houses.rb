ff6057cb0fcdc3881a0e5d3a2381a6e8bcfe3e85
Mika88 <mikaspivak@gmail.com>
2018-10-14 23:22:36 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_date
-      t.datetime :closing_time
+      t.datetime :closing_date
       t.string :description
     end
   end
